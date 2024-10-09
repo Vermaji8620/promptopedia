ffa307101089d4a1e96ad09b6d1dfd18dd28dd88
AdityaRaj <xyz@gmai.com>
2024-10-09 16:39:34 +0530

    prompt create functionality done

---
+++ app/api/prompt/route.js
@@ -0,0 +1,12 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (req) => {
+  try {
+    await connectToDB();
+    const prompt = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all response", { status: 500 });
+  }
+};
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  console.log(data);
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          post={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPost(data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <form action="" className="w-full relative flex-center">
+        <input
+          type="text"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+          placeholder="Search for text or prompt or username"
+        />
+      </form>
+
+      <PromptCardList data={post} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
